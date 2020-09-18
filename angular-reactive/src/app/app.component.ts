b6a19e74a9d0d29aad84fa89ebd22edb80538f8a
Himanshu Gupta <Himanshu.Gupta14@aexp.com>
2020-09-19 00:52:43 +0530

    Movie changes

--- angular-reactive/src/app/app.component.ts
+++ angular-reactive/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quarkus Movie Application';
+  title = 'Movie Application';
 }
